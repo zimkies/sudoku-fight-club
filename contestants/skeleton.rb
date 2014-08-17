951fa8c0a31f2e3cff848a0c2fdd78b614523ede
Benjamin Kies <ben@joingrouper.com>
2014-08-17 18:48:06 -0400

    Add a skeleton solver as a demo

---
+++ contestants/skeleton.rb
@@ -0,0 +1,20 @@
+#
+# Ben's basic solver
+#
+class SkeletonSolver
+
+  # Takes a sudoku represented by 81 character string.
+  #
+  # The string is read left to right, top to bottom.
+  # The digits 0-8 represent filled in sudoku digits
+  # The character '-' represents a blank space
+  # eg
+  # Example puzzle input "1--856-72---3124--52-470631-132-5--62--7-83156751342800675418234520-316738162-504"
+  def initialize(board)
+  end
+
+  # Returns a string representing the solved solution according to the
+  # above format
+  def solve
+  end
+end
